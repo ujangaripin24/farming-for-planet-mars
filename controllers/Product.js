10d8f7e87de590cdfe280853b1b395154f055b9c
ujangaripin24 <ujangarifin06@gmail.com>
2022-12-05 20:41:50 +0700

    13. upload product to database has been working

--- controllers/Product.js
+++ controllers/Product.js
@@ -70,16 +70,16 @@ export const getProductById = async (req, res) => {
 
 export const createProduct = (req, res) => {
     if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});
-    const {
-        price = req.body.price,
-        name = req.body.title,
-        file = req.files.file,
-        fileSize = file.data.length,
-        ext = path.extname(file.name),
-        fileName = file.md5 + ext,
-        url = `${req.protocol}://${req.get("host")}/images/${fileName}`,
-        allowedType = ['.png','.jpg','.jpeg'],
-    } = req.body;
+    
+    const  price = req.body.price;
+    const  name = req.body.name;
+    const  stock = req.body.stock;
+    const  file = req.files.file;
+    const  fileSize = file.data.length;
+    const  ext = path.extname(file.name);
+    const  fileName = file.md5 + ext;
+    const  url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    const  allowedType = ['.png','.jpg','.jpeg'];
  
     if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
     if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
@@ -87,13 +87,7 @@ export const createProduct = (req, res) => {
     file.mv(`./public/images/${fileName}`, async(err)=>{
         if(err) return res.status(500).json({msg: err.message});
         try {
-            await Product.create({
-                name: name,
-                price: price,
-                image: fileName,
-                url: url,
-                userId: req.userId
-            });
+            await Product.create({ name: name, price: price, image: fileName, stock: stock,url: url, userId: req.userId });
             res.status(200).json({msg: "Product Created Successfuly"});
         } catch (error) {
             console.log(error.message);
