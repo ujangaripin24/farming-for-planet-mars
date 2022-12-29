35b93f94f3a2ee523a7411036b60c02bb460db10
ujangaripin24 <ujangarifin06@gmail.com>
2022-12-29 10:55:16 +0700

    21. fix spaceship engine motor

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -2,14 +2,14 @@ import  User from '../models/UserModel.js';
 
 export const verifyUser = async(req, res, next) => {
     if(!req.session.userId){
-        return res.status(401).json({msg: "You not allowed!!!"});
+        return res.status(401).json({msg: "Anda tidak di izinkan!!!"});
     }
     const user = await User.findOne({
       where:{
         uuid: req.session.userId
       }  
     });
-    if(!user) return res.status(404).json({msg: "User not found"});
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
     req.userId = user.id;
     req.role = user.role;
     next();
