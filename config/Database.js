ceaf52a83361a7ee0ae631d13399f039b59425da
Ujang Aripin <117837188+ujangaripin24@users.noreply.github.com>
2023-01-03 07:51:32 +0000

    22. migrate to supabase

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('nodejs_proyek_13', 'necronomicon',  '123456789', {
-    host: "localhost",
-    dialect: "postgres",
-});
+// const db = new Sequelize('nodejs_proyek_13', 'necronomicon',  '123456789', {
+//     host: "localhost",
+//     dialect: "postgres",
+// });
 
 // const db = new Sequelize('proyek_3', 'root', '',{
 //     host: 'localhost',
 //     dialect: 'mysql',
 // });
+
+const db = new Sequelize('postgres', 'postgres',  'necronomiconbookofdeadlevel3', {
+    host: "db.rgmkgkncywrqhhijtwmn.supabase.co",
+    dialect: "postgres",
+});
 export default db;
